8273e96018fddc35977b9c7619462072cbe6ae2d
Astrako <apamarin@gmail.com>
2020-01-24 10:22:05 +0100

    universal7870: add offline charging support

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -144,6 +144,15 @@ TARGET_POWERHAL_VARIANT := samsung
 # Libhwui
 HWUI_COMPILE_FOR_PERF := true
 
+#Offline charge
+BOARD_CHARGING_MODE_BOOTING_LPM := "/sys/class/power_supply/battery/batt_lp_charging"
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
+
+# OMX
+TARGET_OMX_LEGACY_RESCALING := true
+
 # Recovery
 BOARD_HAS_DOWNLOAD_MODE := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.recovery
