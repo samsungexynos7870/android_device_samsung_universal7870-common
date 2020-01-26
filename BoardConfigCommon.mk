25dc599c47ab82d6ab5e295022887b4c98ba5cc6
Astrako <apamarin@gmail.com>
2020-01-26 20:29:32 +0100

    universal7870: change default WiFi country code to '00'
    * Since WiFi only tablets don't have a SIM slot, Android is unable to detect appropriate-ish country code thus WiFi would end up using some   kind of default/fallback one. That'd prevent us from doing things like scanning for 2.4GHz CH12-14 networks or using higher TX power.
    * Hardcoding WiFi country code to 00 (world) lets us ignore these regulations.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,6 +54,7 @@ BUILD_BROKEN_PHONY_TARGETS := true
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+BOARD_KERNEL_CMDLINE := androidboot.wificountrycode=00
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_IMAGE_NAME := Image
