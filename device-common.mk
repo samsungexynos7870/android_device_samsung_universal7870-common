4cbdd7afe363b36ec036a4547cb6eb896273e9e9
Astrako <apamarin@gmail.com>
2020-01-28 08:58:24 +0100

    universal7870: clean DRM build

--- device-common.mk
+++ device-common.mk
@@ -140,13 +140,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0 \
-    android.hardware.drm@1.1 \
-    libfwdlockengine \
-    libdrmclearkeyplugin \
+    android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.2-service.clearkey
 
 # Ebtables
 PRODUCT_PACKAGES += \
