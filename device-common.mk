3b00e466fe3759e1b8f430cd3bce9289a13ed8e4
Astrako <apamarin@gmail.com>
2020-01-28 08:55:32 +0100

    universal7870: uprev Audio to 5.0

--- device-common.mk
+++ device-common.mk
@@ -75,13 +75,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio@5.0-impl \
-    android.hardware.audio.common@5.0 \
-    android.hardware.audio.common@5.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
