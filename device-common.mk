4a9c08a69749cb6ee47e2986dcf4cfd9e3c5f888
Astrako <apamarin@gmail.com>
2020-01-28 08:57:23 +0100

    universal7870: uprev Camera to 2.5

--- device-common.mk
+++ device-common.mk
@@ -100,8 +100,12 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5-service \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    camera.device@3.3-impl \
+    camera.device@3.4-impl \
+    camera.device@3.5-impl \
     libion_exynos \
     Snap
 
