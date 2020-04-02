0e6599efabfa00794838a0f4b8d3ad2b076c32e3
Astrako <apamarin@gmail.com>
2020-04-02 11:39:53 +0200

    universal7870: uprev radio.config to 1.2

--- device-common.mk
+++ device-common.mk
@@ -211,7 +211,7 @@ PRODUCT_PACKAGES += \
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
-    android.hardware.radio.config@1.1 \
+    android.hardware.radio.config@1.2 \
     libxml2
 
 # Ramdisk
