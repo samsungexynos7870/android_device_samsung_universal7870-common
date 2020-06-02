84e79a19018b2daad44ec0d5f398e0376f137915
Alejandro <apamarin@gmail.com>
2020-06-02 14:28:23 +0200

    universal7870: don't build drm clearkey service

--- device-common.mk
+++ device-common.mk
@@ -131,8 +131,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.0-service
 
 # Flat device tree for boot image
 PRODUCT_HOST_PACKAGES += \
