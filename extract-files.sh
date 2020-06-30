79420962d21179891651ae9d117b7fde85608e3b
Alejandro <apamarin@gmail.com>
2020-06-30 20:57:01 +0200

    universal7870: fix typo

--- extract-files.sh
+++ extract-files.sh
@@ -26,7 +26,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 LINEAGE_ROOT="${MY_DIR}"/../../..
 
-HELPER="${AOSP_ROOT}/vendor/aosp/build/tools/extract_utils.sh"
+HELPER="${AOSP_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -60,7 +60,7 @@ if [ -z "${SRC}" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${AOSP_ROOT}" true "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
