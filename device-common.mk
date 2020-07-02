e5b3cf6d3af700f9a795bcccbf82d156429682b7
Alejandro <apamarin@gmail.com>
2020-07-02 16:08:49 +0200

    universal7870: build sensor@1.0-impl.samsung

--- device-common.mk
+++ device-common.mk
@@ -239,7 +239,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl.samsung \
     android.hardware.sensors@1.0-service
 
 # Thermal
