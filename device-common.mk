05fc20cd3e4c7131ba7520ab3ceb4dd5b30d30e7
Alejandro <apamarin@gmail.com>
2020-08-19 17:41:48 +0200

    universal7870: build android.hardware.thermal@2.0-service.samsung instead generic

--- device-common.mk
+++ device-common.mk
@@ -242,8 +242,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@2.0-service.samsung
     
 # Touchscreen
 PRODUCT_COPY_FILES += \
