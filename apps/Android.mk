f7dc678f7093932ec48040309ebee226b146c93c
Brian Gotte <bngotte@gmail.com>
2021-09-03 14:28:42 -0300

    [TEMP] Add killcamera app

--- apps/Android.mk
+++ apps/Android.mk
@@ -16,5 +16,19 @@
 
 LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := killcamera
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
+
+
