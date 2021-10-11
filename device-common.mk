1b8e2a3d1054257ddb0ee6e14734bde3e41a55c2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 00:46:51 +0200

    universal7870: Uprev clearkey DRM HIDL to 1.4
    Change-Id: Id0895fee70495913f57bc8408ea433660467a7c7

--- device-common.mk
+++ device-common.mk
@@ -122,7 +122,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # Flat device tree for boot image
 PRODUCT_HOST_PACKAGES += \
