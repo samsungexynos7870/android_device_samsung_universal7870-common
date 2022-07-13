4e6b0c4ce928e9cf2c330676ed5ec5cb2b430009
Flominator <github.flominatorgd@gmail.com>
2022-07-13 14:54:54 +0200

    universal7870-common: minor fixup

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -102,7 +102,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.stagefright.omx_default_rank.sw-audio=1 \
-    debug.stagefright.omx_default_rank=0
+    debug.stagefright.omx_default_rank=0 \
     debug.stagefright.ccodec=0
 
 # Wifi
