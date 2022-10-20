ce10e0486f401efa7c787b2bb86f63759e38b3c5
Flominator <github.flominatorgd@gmail.com>
2022-10-20 15:37:15 +0200

    universal7870-common: downgrade to keymaster to 4.0
    this avoids VINTF parse error on manifest target-level="3"

--- device-common.mk
+++ device-common.mk
@@ -155,7 +155,7 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-impl \
-    android.hardware.keymaster@4.1-service
+    android.hardware.keymaster@4.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
