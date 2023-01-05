82b44ec96642dc5039195f7f89c385ceab7ae85b
FlominatorGD <github.flominatogd@gmail.com>
2023-01-05 05:53:57 +0100

    universal7870-common: add an arm only version of android.hardware.sensors@1.0-service called android.hardware.sensors@1.0-service.universal7870 to handle our 32 bit proprietary sensor blobs

--- device-common.mk
+++ device-common.mk
@@ -258,7 +258,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-service.universal7870 \
     libsensorndkbridge \
     libshim_sensorndkbridge
 
