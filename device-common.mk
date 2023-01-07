adcef2492c7c7da31790edc54861a2db0da9d43d
Flominator <github.flominatorgd@gmail.com>
2023-01-07 18:13:25 +0100

    universal7870-common: revert: remove soundtrigger hal

--- device-common.mk
+++ device-common.mk
@@ -70,6 +70,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
     audio.usb.default \
     audio.primary.universal7870_32 \
