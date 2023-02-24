9f2560bb524c7506106aeec939738ff51b4e5704
Flominator <github.flominatorgd@gmail.com>
2023-02-24 22:12:03 +0100

    universal7870-common: remove redundant TARGET_USERIMAGES_USE_F2FS := true

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,7 +80,6 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 # Use these flags if the board has a ext4 partition larger than 2gb
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
 TARGET_FS_CONFIG_GEN := $(LOCAL_PATH)/config.fs
 
 # Partitions
