eb2534f04b8d84e75329ea83aa50c8a797373fdf
Flominator <github.flominatorgd@gmail.com>
2023-07-22 22:26:00 +0200

    universal7870-common: drop explicit android.hardware.drm@1.3.vendor

--- device-common.mk
+++ device-common.mk
@@ -176,8 +176,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.3-service.clearkey
 
 # WideVine DRM setup
 PRODUCT_PROPERTY_OVERRIDES += \
