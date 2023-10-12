ecf7cc727a31a8d039240cf0c611eecc127b8d30
Flominator <github.flominatorgd@gmail.com>
2023-10-12 17:02:47 +0200

    universal7870-common: drop boardcastradio

--- device-common.mk
+++ device-common.mk
@@ -325,12 +325,6 @@ PRODUCT_PACKAGES += \
     e2fsck_static \
     resize2fs_static
 
-# Radio (broadcastradio)
-PRODUCT_PACKAGES += \
-    android.hardware.broadcastradio@1.0-impl \
-    android.hardware.broadcastradio@1.0 \
-    android.hardware.broadcastradio@1.1
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.samsungexynos7870 \
