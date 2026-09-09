5000700bf2388310ed862d567749c427aef11567
Flominator <github.flominatorgd@gmail.com>
2026-09-09 18:46:00 +0000

    universal7870-common: ship oss cbd
    Ported from lineage-18.1-lts commit 3fb812a83e1f9063bc8e341f77edf9dc576ad093
    ("universal7870-common: ship oss cbd"), adapted to the lineage-20 Radio
    block layout (cbd added next to secril_config_svc).
    
    Signed-off-by: Flominator <github.flominatorgd@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -364,6 +364,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     secril_config_svc \
+    cbd \
     libcutils_shim_vendor
 
 PRODUCT_PACKAGES += \
