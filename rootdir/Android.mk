17ce67400c1eb65b3bc363952bf97953994ff512
Flominator <github.flominatorgd@gmail.com>
2026-09-12 06:09:51 +0000

    universal7870-common: add init scripts for the Widevine 1.2 HAL and key provisioning
    Both files are needed once the DRM blobs come from the j7duolte firmware:
    
    * android.hardware.drm@1.2-service.widevine.rc starts the prebuilt widevine
      service for the widevine instances of drm@1.0/1.1/1.2 and creates
      /data/vendor/mediadrm.  The stock version also starts
      /system/bin/move_widevine_data.sh and delete_prov_files.sh, both are gone
      from AOSP since Android 10 and are left out here.
    * vendor.samsung.hardware.security.widevine.keyprovisioning@1.0-service.rc
      starts the Samsung provisioning HAL (class core, user/group system) which
      execs /vendor/bin/wvkprov to talk to the TEE.
    
    The vendor/etc/init paths match what the stock vendor image uses, so the
    modules can simply be added to PRODUCT_PACKAGES.
    
    Signed-off-by: Flominator <github.flominatorgd@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -78,3 +78,19 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.samsungexynos7870.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := android.hardware.drm@1.2-service.widevine.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/android.hardware.drm@1.2-service.widevine.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := vendor.samsung.hardware.security.widevine.keyprovisioning@1.0-service.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/vendor.samsung.hardware.security.widevine.keyprovisioning@1.0-service.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
